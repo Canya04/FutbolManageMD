b58fd6976c958ff38be6e5dac8647e1de0c2028b
Marc <mcanabate.dam@institutcampalans.net>
2024-03-09 17:19:46 +0100

    Vistes Fills de El teu Equip
    He creat i fet el funcionament de canvi de vista de les vistes fills de El Teu Equip

--- FutbolManageMD/FutbolManageMD/ViewModels/ElTeuEquipViewModel.cs
+++ FutbolManageMD/FutbolManageMD/ViewModels/ElTeuEquipViewModel.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FutbolManageMD.ViewModels
 {
     public class ElTeuEquipViewModel : ViewModelBase
     {
+        //Variables
+        private ViewModelBase _vistaFillaActual;
+
+        public ViewModelBase VistaFillaActual
+        {
+            get
+            {
+                return _vistaFillaActual;
+            }
+            set
+            {
+                _vistaFillaActual = value;
+                OnPropertyChanged(nameof(VistaFillaActual));
+            }
+        }
+
+        //--> Commands
+        public ICommand MostrarJugadorsViewCommand { get; }
+        public ICommand MostrarSolicitudEquipViewCommand { get; }
+
+        public ElTeuEquipViewModel()
+        {
+            //Inicialitzar commands
+            MostrarJugadorsViewCommand = new ViewModelCommand(ExecutarMostrarJugadorsViewCommand);
+            MostrarSolicitudEquipViewCommand = new ViewModelCommand(ExecutarMostrarSolicitudEquipViewCommand);
+
+            //Vista Predeterminada
+            ExecutarMostrarJugadorsViewCommand(null);
+        }
+
+        //Mostrar JugadorsView
+        private void ExecutarMostrarJugadorsViewCommand(object obj)
+        {
+            VistaFillaActual = new JugadorsViewModel();
+        }
+
+        //Mostrar SolicitudsEquipView
+        private void ExecutarMostrarSolicitudEquipViewCommand(object obj)
+        {
+            VistaFillaActual = new SolicitudsEquipViewModel();
+        }
     }
 }
